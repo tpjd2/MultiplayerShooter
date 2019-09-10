d3a64ac6426dd2ab133471c70ae49b38a904f976
SENACEDU\salmo.msjunior <salmo.sjunior@sp.senac.br>
2019-09-10 15:35:22 -0300

    desenvolvimento do tutorial até a movimentação do player: video 6

---
+++ MultiplayerShooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float velocidadeRotacao;
+    [SerializeField] float velocidade;
+    float x, y;
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal") * Time.deltaTime * velocidadeRotacao;
+        y = Input.GetAxis("Vertical") * Time.deltaTime * velocidade;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, y);
+    }
+}
